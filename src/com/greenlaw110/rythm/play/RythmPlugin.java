1bb9a0b8a386495395fc1c3ff3caec7711a262a9
Green Luo <greenlaw110@gmail.com>
2012-06-24 17:58:37 +1000

    update dependencies.yml

--- src/com/greenlaw110/rythm/play/RythmPlugin.java
+++ src/com/greenlaw110/rythm/play/RythmPlugin.java
@@ -50,7 +50,7 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class RythmPlugin extends PlayPlugin {
-    public static final String VERSION = "1.0.0-RC6";
+    public static final String VERSION = "1.0.0-RC7";
     public static final String R_VIEW_ROOT = "app/rythm";
 
     public static void info(String msg, Object... args) {
