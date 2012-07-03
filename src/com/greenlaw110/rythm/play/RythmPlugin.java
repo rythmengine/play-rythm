0456760a1590954cc2bfb6133d3e06c36be5b849
Green Luo <greenlaw110@gmail.com>
2012-07-03 22:21:16 +1000

    support to_string mode

--- src/com/greenlaw110/rythm/play/RythmPlugin.java
+++ src/com/greenlaw110/rythm/play/RythmPlugin.java
@@ -50,7 +50,7 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class RythmPlugin extends PlayPlugin {
-    public static final String VERSION = "1.0.0-20120703";
+    public static final String VERSION = "1.0.0-20120704";
     public static final String R_VIEW_ROOT = "app/rythm";
 
     public static void info(String msg, Object... args) {
