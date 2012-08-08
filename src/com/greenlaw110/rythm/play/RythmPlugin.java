232b3ca6de7cd83e8f11cd9914f232d9efc4ede5
Green Luo <greenlaw110@gmail.com>
2012-08-09 09:30:02 +1000

    document update for 20120808 version

--- src/com/greenlaw110/rythm/play/RythmPlugin.java
+++ src/com/greenlaw110/rythm/play/RythmPlugin.java
@@ -52,7 +52,7 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class RythmPlugin extends PlayPlugin {
-    public static final String VERSION = "1.0.0-20120806b";
+    public static final String VERSION = "1.0.0-20120808";
     public static final String R_VIEW_ROOT = "app/rythm";
 
     public static void info(String msg, Object... args) {
