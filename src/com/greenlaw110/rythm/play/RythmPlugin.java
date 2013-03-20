be78eef0fcec27ef536808a1b3da8cf499e8c6f8
Green Luo <greenlaw110@gmail.com>
2013-03-20 21:52:10 +1100

    catch up to latest engine for Sandbox and JSON issue fixes

--- src/com/greenlaw110/rythm/play/RythmPlugin.java
+++ src/com/greenlaw110/rythm/play/RythmPlugin.java
@@ -44,7 +44,7 @@ import java.net.URL;
 import java.util.*;
 
 public class RythmPlugin extends PlayPlugin {
-    public static final String VERSION = "1.0-b5f";
+    public static final String VERSION = "1.0-b5h";
     public static final String R_VIEW_ROOT = "app/rythm";
 
     public static void info(String msg, Object... args) {
@@ -226,8 +226,8 @@ public class RythmPlugin extends PlayPlugin {
         p.put("rythm.codegen.compact.enabled", isProd);
         p.put("rythm.engine.class_loader.parent", playAppClassLoader);
         p.put("rythm.engine.load_precompiled.enabled", Play.usePrecompiled);
-        p.put("rythm.log.source.template.enabled", isProd);
-        p.put("rythm.log.source.java.enabled", isProd);
+        p.put("rythm.log.source.template.enabled", true);
+        p.put("rythm.log.source.java.enabled", true);
         p.put("rythm.engine.precompile.mode", Play.mode.isProd() && System.getProperty("precompile") != null);
         if (Play.usePrecompiled || Play.getFile("precompiled").exists()) {
             File preCompiledRoot = new File(Play.getFile("precompiled"), "rythm");
